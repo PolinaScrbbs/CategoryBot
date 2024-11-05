1d00cde581576b82bae8e3f9962bb3a6932d5696
PolinaScrbbs <147543684+PolinaScrbbs@users.noreply.github.com>
2024-11-05 09:39:56 +0300

    Auth

---
+++ api/user/models.py
@@ -0,0 +1,100 @@
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+
+from fastapi import HTTPException, status
+import jwt
+import bcrypt
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy.ext.asyncio import AsyncSession
+from enum import Enum as BaseEnum
+
+from config import SECRET_KEY
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class BaseEnum(BaseEnum):
+    @classmethod
+    async def get_values(cls):
+        return [breed.value for breed in cls]
+
+
+class Role(BaseEnum):
+    USER = "Пользователь"
+    CASHIER = "Кассир"
+    ADMIN = "Администратор"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20), unique=True, nullable=False)
+    hashed_password = Column(String(512), nullable=False)
+    role = Column(Enum(Role), default=Role.USER, nullable=False)
+    full_name = Column(String(40), nullable=False)
+
+    token = relationship("Token", back_populates="user")
+
+    async def set_password(self, password: str) -> None:
+        self.hashed_password = bcrypt.hashpw(
+            password.encode("utf-8"), bcrypt.gensalt()
+        ).decode("utf-8")
+
+    async def check_password(self, password: str) -> bool:
+        return bcrypt.checkpw(
+            password.encode("utf-8"), self.hashed_password.encode("utf-8")
+        )
+
+    async def generate_token(self, expires_in: int = 4800) -> str:
+        payload = {
+            "user_id": self.id,
+            "exp": datetime.now(timezone.utc) + timedelta(seconds=expires_in),
+        }
+        print(f"SECRET_KEY: {SECRET_KEY} (type: {type(SECRET_KEY)})")
+        return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+
+class Token(Base):
+    __tablename__ = "tokens"
+
+    id = Column(Integer, primary_key=True)
+    token = Column(String(256), unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="token")
+
+    async def verify_token(self, session: AsyncSession, user: Optional[User]):
+        try:
+            jwt.decode(self.token, SECRET_KEY, algorithms=["HS256"])
+            return status.HTTP_200_OK, "Токен верефицирован", self
+
+        except jwt.ExpiredSignatureError:
+            return await self.refresh_token(session, user)
+
+        except jwt.InvalidTokenError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Неправильный токен",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+    async def refresh_token(self, session: AsyncSession, user: Optional[User]):
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Токен истёк",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        new_token = await user.generate_token()
+
+        self.token = new_token
+        session.add(self)
+        await session.commit()
+
+        return status.HTTP_200_OK, "Токен обновлён", self
--- config.py
+++ config.py
@@ -1,7 +1,18 @@
-import os
 from dotenv import load_dotenv
+import os
+
+os.environ.pop("DB_USER", None)
+os.environ.pop("DB_PASSWORD", None)
+os.environ.pop("DB_NAME", None)
+
+os.environ.pop("DATABASE_URL", None)
+os.environ.pop("SECRET_KEY", None)
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
